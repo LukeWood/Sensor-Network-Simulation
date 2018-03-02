0e68c5071d4dd92d06fc7414ca2191545ab5c0a3
Luke Wood <lukewoodcs@gmail.com>
2018-03-02 14:06:48 -0600

    Square plot

--- square.py
+++ square.py
@@ -1,3 +1,8 @@
 from generate_graph import unit_square_graph
 
 adjacency_list, radius, total_edges, average_degree, max_degree, min_degree, edge_distribution = unit_square_graph(100, 10)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.distplot(edge_distribution)
+plt.show()
