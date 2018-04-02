3606b8ac7b3507f5fc572f00ba7991d2e336cab2
Luke Wood <lukewoodcs@gmail.com>
2018-04-01 22:16:55 -0500

    scatterplot

--- lib/rgg/charts/scatterplot3D.py
+++ lib/rgg/charts/scatterplot3D.py
@@ -2,7 +2,6 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def scatterplot3D(nodes, output_dir=""):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
