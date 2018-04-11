97bad6571c3df7b60137cfcc5d9c3333e828182d
Luke Wood <lukewoodcs@gmail.com>
2018-04-10 21:41:44 -0500

    walkthrough visualization is done

--- main_drivers/walkthrough_vis.py
+++ main_drivers/walkthrough_vis.py
@@ -60,7 +60,7 @@ def compute_ordering(adj_list):
         else:
             current_state.append(adj_list[index])
     ordering_states.append(current_state)
-    return ordering, ordering_states
+    return ordering[::-1], ordering_states
 
 def draw_ordering_graph(ordering_states, positions):
     nodes = {}
@@ -90,34 +90,33 @@ def draw_ordering_graph(ordering_states, positions):
     plt.suptitle("Visualization of the removal of nodes during SLVO")
     plt.savefig("../results/walkthrough/ordering.png", bbox_inches="tight")
 
-def draw_coloring_graph(coloring_order, positions):
+
+def draw_coloring_graph(adj_list, coloring_order, positions, coloring):
+    colors = ['#6A1B9A', '#D50000', '#AEEA00', '#00B0FF', '#00B8D4', '#00BFA5', '#00C853', '#DD2C00', '#FFFF00']
     nodes = {}
     plot_index=0
     fig, axes = plt.subplots(4, 5)
     x = 0
     y = 0
-    for state in ordering_states[:-1]:
-        G = nx.Graph()
-        pos = {}
-        for index, position in enumerate(positions):
-            if state[index] is False:
-                continue
-            pos[index]=position
-            G.add_node(index, pos=position, size=1)
-        for node, neighbors in enumerate(state):
-            if neighbors is False:
-                continue
-            for neighbor in neighbors:
-                G.add_edge(node, neighbor)
-        nx.draw(G, pos=pos,node_color="blue", ax=axes[y, x], node_size=50)
+    node_colors=["#000000" for node in positions]
+    G = nx.Graph()
+    pos = {}
+    for index, position in enumerate(positions):
+        pos[index]=position
+        G.add_node(index, pos=position, size=1)
+    for node, neighbors in enumerate(adj_list):
+        for neighbor in neighbors:
+            G.add_edge(node, neighbor)
+    for node in coloring_order:
+        node_colors[node] = colors[coloring[node]]
+        nx.draw(G, pos=pos,node_color=node_colors, ax=axes[y, x], node_size=50)
         if x == 4:
             x = 0
             y = y + 1
         else:
             x = x + 1
     plt.suptitle("Coloring of Graph during SLVO Coloring")
-    plt.savefig("../results/walkthrough/ordering.png", bbox_inches="tight")
-
+    plt.savefig("../results/walkthrough/coloring.png", bbox_inches="tight")
 
 def draw_uncolored(adj_list, positions):
     plt.clf()
@@ -152,7 +151,7 @@ def draw_colored(adj_list, positions, coloring):
 
     plt.title("Colored Unit Square Graph with N=20, R=.4")
     nx.draw(G, pos=pos, node_color=color_map)
-    plt.savefig("../results/walkthrough/colored.png", bbox_inches="tight")
+    plt.savefig("../results/walkthrough/final_colored.png", bbox_inches="tight")
 
 if __name__ == "__main__":
     adj_list, positions = unit_square_graph(N, A, return_positions=True)
@@ -163,3 +162,4 @@ if __name__ == "__main__":
     draw_colored(adj_list, positions, coloring)
 
     draw_ordering_graph(ordering_states, positions)
+    draw_coloring_graph(adj_list, ordering, positions, coloring)
